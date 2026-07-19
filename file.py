fc526f904a363402d0052598fd37df6760963be4
PolipilliBhargavi <bhargavipolipilli1999@gmail.com>
2026-07-19 22:39:19 +0530

    created file

---
+++ file.py
@@ -0,0 +1,17 @@
+with open("login.txt") as f:
+   print(f.read())
+with open("login.txt", "w") as f:
+    f.write("Deployment completed")
+with open("login.txt", "a") as f:
+    f.write("\nSuccess")
+    f.write("\nDeployment completed successfully")
+
+try:
+    with open("logout.txt") as f:
+        print(f.read())
+except FileNotFoundError:
+    print("file is not present in the directory")
+       
+with open("createfile.txt", "x") as f:
+    f.write("New file has been created")
+
